1096200024dd47ea380ab517c6e081721828f162
kkang123 <one98kr@gmail.com>
2025-03-21 13:17:56 +0900

    feat : 액세스 토큰 만료시간 60분 설정 및 만료 시 로컬 스토리지 삭제를 통한 로그아웃 자동 진행 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { useEffect } from "react";
+
+import useUserStore from "./zustand/userStore";
 import AppRouter from "./router/AppRouter";
 import "./App.css";
 
 function App() {
+  const checkTokenExpiry = useUserStore((state) => state.checkTokenExpiry);
+  const initializeFromStorage = useUserStore(
+    (state) => state.initializeFromStorage
+  );
+
+  useEffect(() => {
+    initializeFromStorage();
+
+    const intervalId = setInterval(() => {
+      checkTokenExpiry();
+    }, 100000);
+
+    return () => clearInterval(intervalId);
+  }, [initializeFromStorage, checkTokenExpiry]);
+
   return <AppRouter />;
 }
 
