ff597065ea2c4c09e15faf2acf318e2deb7f5006
kkang123 <one98kr@gmail.com>
2025-03-21 13:56:15 +0900

    feat : header 추가 -> 로그인 로그아웃 버튼 추가 완료

--- src/components/sections/Title.tsx
+++ src/components/sections/Title.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 
 import { SectionProps } from "../../interfaces/sectionprops";
 import IcLineArrow from "../../assets/icon/IcLinekArrow";
+import Header from "../Header/Header";
 
 export const Title: React.FC<SectionProps> = ({ isActive, onClick, id }) => {
   const [showScrollIndicator, setShowScrollIndicator] = useState(false);
@@ -21,37 +22,40 @@ export const Title: React.FC<SectionProps> = ({ isActive, onClick, id }) => {
   }, []);
 
   return (
-    <div
-      id={id}
-      className={`h-screen w-full flex flex-col justify-center items-center bg-gray-900 transition-opacity duration-500 cursor-pointer ${
-        isActive ? "opacity-100" : "opacity-50"
-      }`}
-      onClick={onClick}
-    >
-      <h2 className="text-3xl font-bold mb-4 text-white">반 복</h2>
-      <p className="text-lg text-center text-white mb-8">
-        해결한 코딩테스트 문제의 링크를 등록하고 <br />
-        1일, 3일, 7일, 21일마다 알림을 받아 반복해서 문제를 해결해보세요!!
-      </p>
+    <>
+      <Header />
+      <div
+        id={id}
+        className={`h-screen w-full flex flex-col justify-center items-center bg-gray-900 transition-opacity duration-500 cursor-pointer ${
+          isActive ? "opacity-100" : "opacity-50"
+        }`}
+        onClick={onClick}
+      >
+        <h2 className="text-3xl font-bold mb-4 text-white">반 복</h2>
+        <p className="text-lg text-center text-white mb-8">
+          해결한 코딩테스트 문제의 링크를 등록하고 <br />
+          1일, 3일, 7일, 21일마다 알림을 받아 반복해서 문제를 해결해보세요!!
+        </p>
 
-      {/* 스크롤 안내 추가 */}
-      {showScrollIndicator && (
-        <div
-          className={`absolute bottom-10 flex flex-col items-center animate-bounce transition-opacity duration-1000 ${
-            fadeIn ? "opacity-100" : "opacity-0"
-          }`}
-        >
-          <p className="text-white text-sm mb-2">
-            아래로 스크롤하거나 클릭하세요
-          </p>
-          <IcLineArrow
-            width="24"
-            height="24"
-            color="white"
-            direction="M2 2L14 14L26 2"
-          />
-        </div>
-      )}
-    </div>
+        {/* 스크롤 안내 추가 */}
+        {showScrollIndicator && (
+          <div
+            className={`absolute bottom-10 flex flex-col items-center animate-bounce transition-opacity duration-1000 ${
+              fadeIn ? "opacity-100" : "opacity-0"
+            }`}
+          >
+            <p className="text-white text-sm mb-2">
+              아래로 스크롤하거나 클릭하세요
+            </p>
+            <IcLineArrow
+              width="24"
+              height="24"
+              color="white"
+              direction="M2 2L14 14L26 2"
+            />
+          </div>
+        )}
+      </div>
+    </>
   );
 };
