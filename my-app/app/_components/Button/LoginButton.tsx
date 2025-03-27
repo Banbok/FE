19ffd691ae1f278252137a9ae646256a080e12a9
kkang123 <one98kr@gmail.com>
2025-03-27 11:48:50 +0900

    style : 스타일 크기 삭제

--- my-app/app/_components/Button/LoginButton.tsx
+++ my-app/app/_components/Button/LoginButton.tsx
@@ -3,11 +3,7 @@
 import Link from "next/link";
 
 const LoginButton = () => {
-  return (
-    <Link href="/login" className="px-4 py-2">
-      로그인
-    </Link>
-  );
+  return <Link href="/login">로그인</Link>;
 };
 
 export default LoginButton;
