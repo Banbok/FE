c2f23731e5ff2159295ef7bdd39e2e0ccd72428b
kkang123 <one98kr@gmail.com>
2025-04-07 15:08:40 +0900

    feat : web speech api를 이용한 음성인식 기능 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
+import { Metadata } from "next";
+
 import Home from "./_components/sections/Home";
+import VoiceNavigation from "./_components/VoiceNavigation";
+
+export const metadata: Metadata = {
+  title: "Banbok",
+  description: "코딩 테스트의 반복 학습을 도와주는 웹 사이트 입니다.",
+  keywords: "코딩, 코테, 코딩테스트, 코딩테스트 학습",
+  openGraph: {
+    title: "Banbok",
+    description: "코딩 테스트의 반복 학습을 도와주는 웹 사이트 입니다.",
+  },
+};
 
 export default function main() {
   return (
-    <div>
+    <main>
       <Home />
-    </div>
+      <VoiceNavigation />
+    </main>
   );
 }
