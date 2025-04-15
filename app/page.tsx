18c4ee99e92ad46c8963f6c8ace8bd74ff88c096
kkang123 <one98kr@gmail.com>
2025-04-15 12:28:22 +0900

    feat : og  개선

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,18 @@ export const metadata: Metadata = {
   openGraph: {
     title: "Banbok",
     description: "코딩 테스트의 반복 학습을 도와주는 웹 사이트 입니다.",
+    url: "https://banbok.vercel.app/",
+    images: [
+      {
+        url: "https://banbok.vercel.app/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Banbok 대표 이미지",
+      },
+    ],
+    siteName: "Banbok",
+    locale: "ko_KR",
+    type: "website",
   },
 };
 
