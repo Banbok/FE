6ab8b5a3aee8fa703b3cb3fab8b5712e2e63b5c7
kkang123 <one98kr@gmail.com>
2025-04-18 14:29:41 +0900

    refactor: 크롤러 로직 모듈화 및 구조 개선

--- app/api/scrape/route.ts
+++ app/api/scrape/route.ts
@@ -1,37 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
-import * as cheerio from "cheerio";
+import { CrawlerManager } from "../../utils/crawlerManager";
 
 export async function POST(req: NextRequest) {
   const { link } = await req.json();
+  const crawlerManager = new CrawlerManager();
 
   try {
-    // const response = await fetch(link);
-    const response = await fetch(link, {
-      headers: {
-        "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
-      },
-    });
-
-    const html = await response.text();
-    const $ = cheerio.load(html);
-
-    console.log("받은 HTML:", html);
-
-    let title = "";
-    let site = "";
-
-    if (link.includes("acmicpc.net")) {
-      title = $("#problem_title").text().trim();
-      site = "Baekjoon";
-    } else if (link.includes("school.programmers.co.kr")) {
-      title = $(".challenge-title").text().trim();
-      site = "Programmers";
-    }
-
-    console.log("추출된 제목:", title);
-
-    return NextResponse.json({ title, link, site });
+    const result = await crawlerManager.crawl(link);
+    return NextResponse.json(result);
   } catch (error) {
     return NextResponse.json(
       { message: "크롤링 실패", error },
