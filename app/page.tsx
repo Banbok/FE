a64a9e47bc15001c6419187def8ff8a9c7ebc182
kkang123 <one98kr@gmail.com>
2025-08-06 22:23:03 +0900

    refactor : 메타 태그 수정

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,9 @@ import { Metadata } from "next";
 import Home from "./_components/sections/Home";
 
 export const metadata: Metadata = {
-  title: "Banbok",
-  description: "코딩 테스트의 반복 학습을 도와주는 웹 사이트 입니다.",
+  title: "Banbok - 코딩테스트 준비를 위한 반복 학습 웹사이트",
+  description:
+    "Banbok은 코딩테스트, 백준, 프로그래머스 이 외 코딩테스트 사이트의 문제를 반복 학습을 도와주는 웹사이트입니다.",
   keywords: "코딩, 코테, 코딩테스트, 코딩테스트 학습",
   openGraph: {
     title: "Banbok",
